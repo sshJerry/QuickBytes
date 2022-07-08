16b918f6e1411cd0c02a2fc3c532747bb485ab33
Kev-Index <kliu0862@bths.edu>
2022-07-08 15:37:11 -0400

    Initial Login Menu

--- src/com/main/QuickBytes.java
+++ src/com/main/QuickBytes.java
@@ -3,6 +3,10 @@ package com.main;
 public class QuickBytes {
 	public static void main(String[] args) {
 		//I got here lfg (james wuz here)
-		System.out.println("Hello!");
+		System.out.println("****QuickBytes****");
+		System.out.println("Login As:");
+		System.out.println("1. Employee");
+		System.out.println("2. Vendor");
+		System.out.println("0. Exit");
 	}
 }
