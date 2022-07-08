6aa6056f6c5a84fc8af086a8d3b276f57dca92dc
Kev-Index <kliu0862@bths.edu>
2022-07-08 15:51:29 -0400

    Initial menu in while true loop

--- src/com/main/QuickBytes.java
+++ src/com/main/QuickBytes.java
@@ -1,12 +1,20 @@
 package com.main;
 
+import com.main.service.UserService;
+
 public class QuickBytes {
 	public static void main(String[] args) {
-		//I got here lfg (james wuz here)
-		System.out.println("****QuickBytes****");
-		System.out.println("Login As:");
-		System.out.println("1. Employee");
-		System.out.println("2. Vendor");
-		System.out.println("0. Exit");
+		UserService userService = new UserService();
+		
+		while (true) {
+			int input = userService.displayMenuAndReadInput();
+			System.out.println(input);
+			
+			if (input == 0) {
+				System.out.println("Exiting... Bye!");
+				break;
+			}
+			
+		}
 	}
 }
---
+++ src/com/main/service/UserService.java
@@ -0,0 +1,22 @@
+package com.main.service;
+
+import java.util.Scanner;
+
+public class UserService {
+	private Scanner sc;
+	{
+		this.sc = new Scanner(System.in);
+	}
+
+	public int displayMenuAndReadInput() {
+		System.out.println("****QuickBytes****");
+		System.out.println("Login As:");
+		System.out.println("1. Employee");
+		System.out.println("2. Vendor");
+		System.out.println("0. Exit");
+		int input = sc.nextInt();
+		
+		return input;
+	}
+
+}
