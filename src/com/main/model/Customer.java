c632f2149449f31c4bd9e0999484f891c2c7e06f
Kev-Index <kliu0862@bths.edu>
2022-07-08 18:06:36 -0400

    Customer fetch and delete functionalities

--- src/com/main/model/Customer.java
+++ src/com/main/model/Customer.java
@@ -8,4 +8,19 @@ public class Customer {
 	private String username;
 	private String password;
 	private float balance;
+	
+	public Customer() {
+	}
+	
+	public Customer(int customerId, int employeeId, String firstName, String lastName, String username, String password,
+			float balance) {
+		this.customerId = customerId;
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.balance = balance;
+		
+	}
 }
--- src/com/main/service/UserService.java
+++ src/com/main/service/UserService.java
@@ -3,6 +3,8 @@ package com.main.service;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.main.db.Database;
+
 public class UserService {
 	private Scanner sc;
 	{
@@ -29,28 +31,30 @@ public class UserService {
 	}
 
 	public void login(int userType) {
+		Database db = new Database();
+		Boolean userIsValid = false;
 		
 		try {
 			System.out.println("\nWelcome!");
 			System.out.println("Please Enter Your Credentials: \n");
-			System.out.print("Username: ");
+			System.out.print("ID: ");
 			sc.nextLine();
-			String username = sc.nextLine();
+			String id = sc.nextLine();
 			System.out.print("Password: ");
 			String password = sc.nextLine();
+			
+			switch(userType) {
+			case 1: 
+				userIsValid = db.validateCustomer(id,password);
+				break;
+			case 2: 
+				userIsValid = db.validateVendor(id,password);
+				break;
+			default: 
+				break;
+			}
 		} catch (InputMismatchException ime) {
 			System.out.println("\n Incorrect Input Type. Please Try Again!");
-		}
-		
-		switch(userType) {
-		case 1: 
-			//validateCustomer();
-			break;
-		case 2: 
-			//validateVendor();
-			break;
-		default: 
-			break;
 		}
 	}
 
